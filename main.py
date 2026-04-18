922748eb23cb762f5ab8560af93aef275b97d329
bjavohir2197 <bjavohir2197@gmail.com>
2026-04-18 12:46:59 +0500

    🚀 Add main application code

---
+++ main.py
@@ -0,0 +1,10 @@
+def main():
+        print("=" * 40)
+    print("Masala: Berilgan yil kabisa yili ekanligini tekshiring.")
+    print("=" * 40)
+    print("\nDastur muvaffaqiyatli ishga tushdi!")
+    print("Masala yechildi.")
+
+
+if __name__ == "__main__":
+    main()
